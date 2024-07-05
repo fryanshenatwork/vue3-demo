4e2d8935def388b6bba4d46afeee25b2bda14702
fryan <26700199+fryanshenatwork@users.noreply.github.com>
2024-07-05 22:47:54 +0800

    feat: Login

---
+++ src/layout/Layout.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import { HomeOutlined } from '@ant-design/icons-vue';
+</script>
+
+<template>
+  <div>
+    <HomeOutlined />
+    <RouterView />
+  </div>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,18 @@
 import { createWebHashHistory, createRouter } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
 
+import Layout from '@/layout/Layout.vue'
 import Login from '@/pages/Login.vue'
 import Home from '@/pages/Home.vue'
 
-const routes = [
-  { path: '/home', component: Home },
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: Layout,
+    children: [
+      { path: 'home', component: Home },
+    ]
+  },
   { path: '/:pathMatch(.*)*', component: Login },
 ]
 
